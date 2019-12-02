255cb41bfe47716c0ca058993eedb4f96ea676ff
omer ozkan <omer.ozkan@outlook.com>
2019-12-02 21:23:53 +0300

    ınıtı
    ı

---
+++ SearchTextUIControl/SearchTextUIControl/SearchBoxUI.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SearchTextUIControl
+{
+    /// <summary>
+    /// Interaction logic for SearchBoxUI.xaml
+    /// </summary>
+    public partial class SearchBoxUI : UserControl
+    {
+
+        public bool showSearch { get; set; }
+
+        public bool showDelete { get; set; }
+        public SearchBoxUI()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
